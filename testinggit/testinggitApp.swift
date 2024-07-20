e66b24f03a6b8e5811d141318e0e616cc6e7bdce
phua hong yu <phua_hong_yu@students.edu.sg>
2024-07-20 09:19:56 +0800

    Initial Commit

---
+++ testinggit/testinggitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testinggitApp.swift
+//  testinggit
+//
+//  Created by Tian peng Phua on 20/7/24.
+//
+
+import SwiftUI
+
+@main
+struct testinggitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
